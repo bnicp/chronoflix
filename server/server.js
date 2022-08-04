1185b449fe6085cf7e4c13e8b03536d427db77ec
Heather Noyes <heatherknoyes@gmail.com>
2022-08-03 22:19:54 -0400

    New commit for start of app

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
 const db = require("./config/connection");
-const routes = require("./routes");
 const { authMiddleware } = require("./utils/auth");
 const { typeDefs, resolvers } = require("./schemas");
 
